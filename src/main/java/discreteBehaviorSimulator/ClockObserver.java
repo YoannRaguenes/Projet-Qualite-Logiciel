d8f413b5ab4d780e60b0fa8f5523c07239d46cc8
YoannRaguenes <58259678+YoannRaguenes@users.noreply.github.com>
2021-03-25 15:27:44 +0100

    Update ClockObserver.java

--- src/main/java/discreteBehaviorSimulator/ClockObserver.java
+++ src/main/java/discreteBehaviorSimulator/ClockObserver.java
@@ -7,13 +7,13 @@ public interface ClockObserver {
 	
 	/**
 	 * notify the user of a clock change
-	 * @param time 
+	 * @param time to notify
 	 */
 	public void clockChange(int time);
 	
 	/**
 	 * notify the user of the next clock change
-	 * @param next jump 
+	 * @param nextJump to notify
 	 */
 	public void nextClockChange(int nextJump);
 }
