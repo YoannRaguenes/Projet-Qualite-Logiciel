92cab17d089167e0f7149806d2f3c2bf1683cc0a
enzo masson <39915492+enzomasson25@users.noreply.github.com>
2021-03-25 14:15:58 +0100

    Update DiscreteActionOnOffDependent.java

--- src/main/java/action/DiscreteActionOnOffDependent.java
+++ src/main/java/action/DiscreteActionOnOffDependent.java
@@ -13,18 +13,18 @@ import timer.Timer;
  */
 public class DiscreteActionOnOffDependent implements DiscreteActionInterface {
 	
-	protected DiscreteActionInterface onAction;
-	protected DiscreteActionInterface offAction;
-	protected DiscreteActionInterface currentAction;
+	protected DiscreteActionInterface onAction; 		// the on action
+	protected DiscreteActionInterface offAction;		// the off action
+	protected DiscreteActionInterface currentAction;	// the current action 
 	
-	private Integer currentLapsTime;
-	private Integer lastOffDelay=0;
+	private Integer currentLapsTime;					// the current LapsTime
+	private Integer lastOffDelay=0;						// delay initialy equals 0 
 	
 	/**
 	 * Construct an On Off dependence, first action (method) called is On, then method nextMethod() is called to select the next action.
 	 * The default behavior of nextMethod() is to switch between On and Off actions.  It can be change by overloading. 
 	 * 
-	 * @param o
+	 * @param o 
 	 * @param on
 	 * @param timerOn
 	 * @param off
@@ -38,12 +38,12 @@ public class DiscreteActionOnOffDependent implements DiscreteActionInterface {
 	}*/
 	
 	/**
-	 * 
-	 * @param o
-	 * @param on
-	 * @param timerOn
-	 * @param off
-	 * @param timerOff
+	 * Constructor
+	 * @param o Object on which methods will be applied
+	 * @param on Method of the onAction
+	 * @param timerOn Timer of the onAction 
+	 * @param off Method of the offAction
+	 * @param timerOff Timer of the offAction
 	 */
 	public DiscreteActionOnOffDependent(Object o, String on, Timer timerOn, String off, Timer timerOff){
 		this.onAction = new DiscreteAction(o, on, timerOn);
@@ -88,6 +88,14 @@ public class DiscreteActionOnOffDependent implements DiscreteActionInterface {
 		
 	}
 	
+	/**
+	 * Constructor
+	 *  @param o Object on which methods will be applied
+	 * @param on Method of the onAction 
+	 * @param datesOn Tree of multiple Timer for the onAction
+	 * @param off Method of the offAction
+	 * @param datesOff Tree of multiple Timer for the offAction 
+	 */
 	public DiscreteActionOnOffDependent(Object o, String on, TreeSet<Integer> datesOn, String off, TreeSet<Integer> datesOff){
 		/*Vector<Integer> timeLapseOn = new Vector<Integer>();
 		Vector<Integer> timeLapseOff = new Vector<Integer>();
@@ -105,7 +113,10 @@ public class DiscreteActionOnOffDependent implements DiscreteActionInterface {
 			this.currentAction = this.offAction;
 		}
 	}
-
+	
+	/**
+	 * set currentAction to be the next action
+	 */
 	public void nextAction(){
 		if (this.currentAction == this.onAction){
 			this.currentAction = this.offAction;
@@ -117,32 +128,63 @@ public class DiscreteActionOnOffDependent implements DiscreteActionInterface {
 			this.currentAction.spendTime(this.lastOffDelay);
 		}
 	}
-
+	
+	/**
+	 * 
+	 * Spend the time of the actualAction 
+	 *
+	 * @param t int how much time you want to spend 
+	 */
 	public	void spendTime(int t) {
 		this.currentAction.spendTime(t);
 	}
 
+	/**
+	 * get the method of the currentAction
+	 * @return Method 
+	 */
 	public Method getMethod() {
 		return this.currentAction.getMethod();
 	}
 
+	/**
+	 * get current LapsTime of the currentAction
+	 * @return Integer
+	 */
 	public Integer getCurrentLapsTime() {
 		return this.currentAction.getCurrentLapsTime();
 	}
 
+	/**
+	 * get the object of the currentAction
+	 * @return Object
+	 */
 	public Object getObject() {
 		return this.currentAction.getObject();
 	}
 
+	/**
+	 * compare the currentAction to a DiscreteActionInterface
+	 * 
+	 * @param c DiscreteActionInterface you want to compare
+	 */
 	public int compareTo(DiscreteActionInterface c) {
 		return this.currentAction.compareTo(c);
 	}
 	
+	/**
+	 * get the next action 
+	 * @return DiscreteActionInterface
+	 */
 	public DiscreteActionInterface next() {
 		this.nextAction();
 		return this;
 	}
 	
+	/**
+	 * test if there is next action or not
+	 * @return Boolean 
+	 */
 	public boolean hasNext() {
 		return this.onAction.hasNext() || this.offAction.hasNext();		
 	}
