b6753e489acbe499e3bef11d53ab6bb773b336eb
Guilly-Deborah <56953554+Guilly-Deborah@users.noreply.github.com>
2021-03-25 14:47:06 +0100

    added javadoc for DateTimer.java

--- src/main/java/timer/DateTimer.java
+++ src/main/java/timer/DateTimer.java
@@ -9,6 +9,11 @@ public class DateTimer  implements Timer {
 	Vector<Integer> lapsTimes;
 	Iterator<Integer> it;
 	
+	/**
+	 * @param dates
+	 * 
+	 * creation with an integer TreeSet of dates 
+	 */
 	public DateTimer(TreeSet<Integer> dates) {
 		this.lapsTimes = new Vector<Integer>();
 		Integer last;
@@ -24,17 +29,29 @@ public class DateTimer  implements Timer {
 
 	}
 	
+	/**
+	 * 
+	 * @param lapsTimes
+	 * 
+	 * creation with an integer Vector of lapsTimes
+	 */
 	public DateTimer(Vector<Integer> lapsTimes) {
 		this.lapsTimes = new Vector<Integer>(lapsTimes);
 		this.it = this.lapsTimes.iterator();
 	}
 
 	@Override
+	/**
+	 * Returns true if there are still elements in "it"
+	 */
 	public boolean hasNext() {
 		return it.hasNext();
 	}
 
 	@Override
+	/**
+	 * Returns next item in "it"
+	 */
 	public Integer next() {
 		return it.next();
 	}
