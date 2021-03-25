5cffdde65cd75a008e8db6baff5fa698ea791ec4
Noah Razafindrabe <noah.razafindrabe@outlook.com>
2021-03-25 14:53:33 +0100

    added javadoc for mergedTimer.java

--- src/main/java/timer/MergedTimer.java
+++ src/main/java/timer/MergedTimer.java
@@ -5,15 +5,27 @@ public class MergedTimer implements Timer{
 	private Timer timer1;
 	private Timer timer2;
 
+	
+	/**
+	 * Construct a merged timer (addition of 2 timer)
+	 * @param timer1 first timer
+	 * @param timer2 second timer
+	 */
 	public MergedTimer(Timer timer1, Timer timer2) {
 		this.timer1 = timer1;
 		this.timer2 = timer2;
 	}
+	/**
+	 * @return if it has a next time
+	 */
 	@Override
 	public boolean hasNext() {
 		return (this.timer1.hasNext() && this.timer2.hasNext());
 	}
 
+	/**
+	 * @return the delay time
+	 */
 	@Override
 	public Integer next() {
 		if(this.hasNext()) {
